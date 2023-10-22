904da667d6d36485510e8286078ddced7af5d906
dorian <dorian@DESKTOP-B4SO0PQ>
2023-10-22 19:02:37 +0200

    1

---
+++ LibApp/Models/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace LibApp.Models
+{
+    public class Book
+    {
+        public string id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+    }
+}

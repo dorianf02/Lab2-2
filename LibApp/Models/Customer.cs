a3b3c1d8a55ea18f2020bd682ceb3376a020b706
dorian.fugat <dorian.fugat@NY-22>
2023-10-28 09:17:41 +0200

    next upd

--- LibApp/Models/Customer.cs
+++ LibApp/Models/Customer.cs
@@ -1,8 +1,11 @@
-﻿namespace LibApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibApp.Models
 {
     public class Customer
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public Customer()
         {

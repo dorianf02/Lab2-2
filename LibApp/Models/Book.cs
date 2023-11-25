3521df7d25064fc63abb2dc47a86d6e3d94dce0c
dorian.fugat <dorian.fugat@NY-22>
2023-11-25 08:59:47 +0100

    update

--- LibApp/Models/Book.cs
+++ LibApp/Models/Book.cs
@@ -1,9 +1,21 @@
-﻿namespace LibApp.Models
+﻿using Humanizer.Localisation;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibApp.Models
 {
     public class Book
     {
-        public string id { get; set; }
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Title { get; set; }
+        [Required]
         public string Author { get; set; }
+        [Required]
+        public Genre Genre { get; set; }
+        public int GenreId { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public byte NumberInStock { get; set; }
     }
 }

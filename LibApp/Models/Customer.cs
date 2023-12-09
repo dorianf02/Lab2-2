81080a6562b3cc5e5fba281340e161eb1a2c5fae
dorian.fugat <dorian.fugat@NY-22>
2023-12-09 09:05:06 +0100

    update

--- LibApp/Models/Customer.cs
+++ LibApp/Models/Customer.cs
@@ -5,8 +5,16 @@ namespace LibApp.Models
     public class Customer
     {
         public int Id { get; set; }
+        // Data Annotation - convention name
         [Required]
+        [StringLength(100)]
         public string Name { get; set; }
+        [Display(Name = "Date of Birth")]
+        public DateTime? Birthdate { get; set; }
+        public bool SubscribedToNewsletter { get; set; }
+        public MembershipType MembershipType { get; set; }
+        [Display(Name = "Membership Type")]
+        public byte MembershipTypeId { get; set; }
         public Customer()
         {
             

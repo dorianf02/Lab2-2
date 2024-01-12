de519daff8b9f228cdb7e58ae48778b4ae2f14d0
dorian <dorian@DESKTOP-B4SO0PQ>
2024-01-12 22:00:27 +0100

    new

--- LibApp/Models/SeedData.cs
+++ LibApp/Models/SeedData.cs
@@ -8,9 +8,9 @@ namespace LibApp.Models
         public static void Initialize(IServiceProvider serviceProvider)
         {
             using (var context =
-               new ApplicationDbContext(
-                   serviceProvider.GetRequiredService<
-                       DbContextOptions<ApplicationDbContext>>()))
+                new ApplicationDbContext(
+                    serviceProvider.GetRequiredService<
+                        DbContextOptions<ApplicationDbContext>>()))
             {
                 // Look for any MembershipTypes
                 if (context.MembershipTypes.Any())
@@ -20,10 +20,10 @@ namespace LibApp.Models
                 }
 
                 context.MembershipTypes.AddRange(
-                    new MembershipType { Id = 1, SignUpFee = 0, DurationInMonths = 0, DiscountRate = 0 },
-                    new MembershipType { Id = 2, SignUpFee = 30, DurationInMonths = 1, DiscountRate = 5 },
-                    new MembershipType { Id = 3, SignUpFee = 90, DurationInMonths = 3, DiscountRate = 10 },
-                    new MembershipType { Id = 4, SignUpFee = 300, DurationInMonths = 12, DiscountRate = 20 }
+                    new MembershipType { Name = "Pay as you go", Id = 1, SignUpFee = 0, DurationInMonths = 0, DiscountRate = 0 },
+                    new MembershipType { Name = "Monthly", Id = 2, SignUpFee = 30, DurationInMonths = 1, DiscountRate = 5 },
+                    new MembershipType { Name = "Quaterly", Id = 3, SignUpFee = 90, DurationInMonths = 3, DiscountRate = 10 },
+                    new MembershipType { Name = "Annualy", Id = 4, SignUpFee = 300, DurationInMonths = 12, DiscountRate = 20 }
                 );
 
                 context.SaveChanges();
